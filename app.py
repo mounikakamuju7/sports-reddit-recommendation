ff5d8fb84d1dfe8f7effedcbc54c8a4534fd24c6
mounikakamuju7 <140903672+mounikakamuju7@users.noreply.github.com>
2026-03-03 11:05:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ import numpy as np
 from sklearn.decomposition import TruncatedSVD
 
 app = FastAPI(title="Sports Reddit News Recommendation API")
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can restrict later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # -----------------------------
 # Load Data
